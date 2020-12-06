27fdd76b9e7b56fe3d3a06bdd87c4b9c026e7c96
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-06 16:34:31 +0100

    test

---
+++ PlaymakerUnity3D/Assets/How_To_Do_Touch_system/How_to_doTouch_system/How_To_Do_touch_system/Playmaker Touch Controls/How_To_Do_GetButtonDown.cs
@@ -0,0 +1,39 @@
+﻿using HutongGames.PlayMaker;
+
+namespace How_To_Do.Playmaker
+{
+    [ActionCategory(How_To_Do_Action_Category.CategoryName)]
+    [Tooltip("Sends an Event when a Button is pressed.")]
+    public class How_To_Do_GetButtonDown : FsmStateAction
+    {
+        [RequiredField]
+        [Tooltip("The name of the button. Set in the Unity Input Manager.")]
+        public FsmString ButtonName;
+
+        [Tooltip("Event to send if the button is pressed.")]
+        public FsmEvent SendEvent;
+
+        [Tooltip("Set to True if the button is pressed.")]
+        [UIHint(UIHint.Variable)]
+        public FsmBool StoreResult;
+
+        public override void Reset()
+        {
+            ButtonName = "Fire1";
+            SendEvent = null;
+            StoreResult = null;
+        }
+
+        public override void OnUpdate()
+        {
+            var buttonDown = How_To_Do_Touch_InputManager.GetButtonDown(ButtonName.Value);
+
+            if (buttonDown)
+            {
+                Fsm.Event(SendEvent);
+            }
+
+            StoreResult.Value = buttonDown;
+        }
+    }
+}
