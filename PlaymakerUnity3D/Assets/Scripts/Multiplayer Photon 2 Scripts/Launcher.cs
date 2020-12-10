b506fd6e4c22858fece065cf1123ea905b5258c0
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-10 11:53:41 +0100

    Added Launcher.cs and set debug info for connecting

---
+++ PlaymakerUnity3D/Assets/Scripts/Multiplayer Photon 2 Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+using Photon.Realtime;
+using System.Linq;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+
+	void Start()
+	{
+		Debug.Log("Connecting to Master");
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to Master");
+		PhotonNetwork.JoinLobby();
+	}
+
+	public override void OnJoinedLobby()
+	{
+		Debug.Log("Joined Lobby");
+	}
+
+    private void Update()
+    {
+        
+    }
+}
