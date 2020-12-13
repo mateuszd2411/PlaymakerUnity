e6a47e4b4c590c5e0393d4cd1e25c3861ebd82e2
mateuszd2411 <mateuszd2411@gmail.com>
2020-12-13 17:45:43 +0100

    Build multiplayer scene and tested create room (OK)

--- PlaymakerUnity3D/Assets/Scripts/Multiplayer Photon 2 Scripts/Launcher.cs
+++ PlaymakerUnity3D/Assets/Scripts/Multiplayer Photon 2 Scripts/Launcher.cs
@@ -8,11 +8,19 @@ using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+
+	public static Launcher Instance;
+
 	[SerializeField] TMP_InputField roomNameInputField;
 	[SerializeField] TMP_Text errorText;
 	[SerializeField] TMP_Text roomNameText;
 	[SerializeField] Transform roomListContent;
 	[SerializeField] GameObject roomListItemPrefab;
+
+	void Awake()
+	{
+		Instance = this;
+	}
 
 	void Start()
 	{
@@ -60,6 +68,12 @@ public class Launcher : MonoBehaviourPunCallbacks
 		MenuManager.Instance.OpenMenu("loading");
 	}
 
+	public void JoinRoom(RoomInfo info)
+	{
+		PhotonNetwork.JoinRoom(info.Name);
+		MenuManager.Instance.OpenMenu("loading");
+	}
+
 	public override void OnLeftRoom()
 	{
 		MenuManager.Instance.OpenMenu("title");
@@ -67,16 +81,16 @@ public class Launcher : MonoBehaviourPunCallbacks
 
 	public override void OnRoomListUpdate(List<RoomInfo> roomList)
 	{
-		/*foreach (Transform trans in roomListContent)
-		{
-			Destroy(trans.gameObject);
-		}
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
 
-		for (int i = 0; i < roomList.Count; i++)
-		{
-			if (roomList[i].RemovedFromList)
-				continue;
-			Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
-		}*/
-	}
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList)
+                continue;
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
 }
